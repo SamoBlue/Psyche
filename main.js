b3a3334aa288c3d8318fcddaf2cb4a3fe533124f
SamoBlue <al_fear_dead@windowslive.com>
2024-03-06 12:25:45 +0100

    Test

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// Function to extract the dominant color from an image
-function extractDominantColor(imagePath, callback) {
-    var img = new Image();
-    img.crossOrigin = "Anonymous";
-    img.src = imagePath;
-    img.onload = function() {
+// Modified extractDominantColor function
+function extractDominantColor(imagePath, callback, postId) {
+  var img = new Image();
+  img.crossOrigin = "Anonymous";
+  img.src = imagePath;
+  img.onload = function() {
       var canvas = document.createElement("canvas");
       var context = canvas.getContext("2d");
       context.drawImage(img, 0, 0, img.width, img.height);
@@ -11,40 +11,40 @@ function extractDominantColor(imagePath, callback) {
       var colorCounts = {};
       var maxCount = 0;
       var dominantColor = null;
-  
+
       for (var i = 0; i < imageData.length; i += 4) {
-        var r = imageData[i];
-        var g = imageData[i + 1];
-        var b = imageData[i + 2];
-        var rgb = r + "," + g + "," + b;
-        if (colorCounts[rgb]) {
-          colorCounts[rgb]++;
-        } else {
-          colorCounts[rgb] = 1;
-        }
-        if (colorCounts[rgb] > maxCount) {
-          maxCount = colorCounts[rgb];
-          dominantColor = rgb;
-        }
+          var r = imageData[i];
+          var g = imageData[i + 1];
+          var b = imageData[i + 2];
+          var rgb = r + "," + g + "," + b;
+          if (colorCounts[rgb]) {
+              colorCounts[rgb]++;
+          } else {
+              colorCounts[rgb] = 1;
+          }
+          if (colorCounts[rgb] > maxCount) {
+              maxCount = colorCounts[rgb];
+              dominantColor = rgb;
+          }
       }
-  
-      callback(dominantColor);
-    };
-  }
-  
-  // Apply the dominant color to the tab
-  function applyTabColor(dominantColor) {
-    var tabs = document.querySelectorAll(".post-tab");
-    tabs.forEach(function(tab) {
-      tab.style.backgroundColor = "rgb(" + dominantColor + ")";
-    });
-  }
-  
-  // Call the functions when the document is ready
-  document.addEventListener("DOMContentLoaded", function() {
-    var imagePaths = ["Images/niijima.jpg", "Images/niijima.jpg"]; // Add image paths here
-    imagePaths.forEach(function(imagePath) {
-      extractDominantColor(imagePath, applyTabColor);
-    });
+
+      // Call the callback function with the dominant color and postId
+      callback(dominantColor, postId);
+  };
+}
+
+// Modify the applyTabColor function to apply the dominant color to the post element
+function applyBackgroundColor(dominantColor, postId) {
+  var post = document.querySelector("#post-image-" + postId).closest(".post");
+  post.style.backgroundColor = "rgb(" + dominantColor + ")";
+}
+
+// Call the functions for each post when the document is ready
+document.addEventListener("DOMContentLoaded", function() {
+  var posts = document.querySelectorAll(".post-image");
+
+  posts.forEach(function(postImage, index) {
+      var imagePath = postImage.src; // Get the image path from the .post-image element
+      extractDominantColor(imagePath, applyBackgroundColor, index);
   });
-  
+});
